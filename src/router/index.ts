b5f7767730b70bdac0ec468601eb66af4a661be9
廖浩龙 <aliaohaolong@gmail.com>
2026-07-14 23:08:48 +0800

    feat: 新增登录页、激活页、404 页路由；新增全局路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from "@/stores/auth.ts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +11,60 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+      meta: {
+        requiresAuth: false, // 使用局部路由守卫进行验证，禁用全局路由守卫验证
+        hideHeader: true,
+        hideFooter: true,
+      },
+      // 局部路由守卫
+      async beforeEnter(to, from, next) {
+        const authStore = useAuthStore()
+        const isLoggedIn: boolean = await authStore.ping(); // 使用 await 等待异步函数返回结果
+
+        if (isLoggedIn) {
+          next({name: 'user'})
+        } else {
+          next()
+        }
+      }
+    },
+    // 激活账户页
+    {
+      path: '/activate/:token',
+      name: 'activate',
+      component: () => import('@/views/ActivateView.vue'),
+      props: true, // 将路径参数作为 props 传递给组件
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/AboutView.vue'),
     },
+    // 匹配所有未定义的路由
+    {
+      path: '/:pathMatch(.*)*', // 通配符路由
+      name: 'not-found',
+      component: () => import('@/views/NotFoundView.vue'),
+    }
   ],
+})
+
+// 全局路由守卫
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 需要登录，进行验证
+    const authStore = useAuthStore()
+    const isLoggedIn: boolean = await authStore.ping();
+
+    if (!isLoggedIn) { // 如果未登录
+      next({ name: 'login' }) // 跳转到登录页
+    }
+  }
+  // 允许导航
+  next()
 })
 
 export default router
