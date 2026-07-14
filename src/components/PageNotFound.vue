7fef3dcfb9697d87e67d00e4f8e3d86f0744e0f7
廖浩龙 <aliaohaolong@gmail.com>
2026-07-14 23:02:50 +0800

    feat: 添加 404 页面

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="flex-1 flex flex-col items-center justify-center">
+    <div class="mb-6 pb-1 px-1 border-b border-(--border)">
+      <h1 class="font-serif text-4xl text-(--muted-foreground)">404</h1>
+    </div>
+    <div class="text-sm text-(--muted-foreground)">
+      <p>页面不存在，<a class="text-(--primary) transition-colors hover:opacity-80" href="/">返回首页</a></p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import PageNotFound from "@/components/PageNotFound.vue";
+</script>
+
+<template>
+  <PageNotFound/>
+</template>
+
+<style scoped>
+
+</style>
