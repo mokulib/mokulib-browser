5b5de5e801eca112b84b055393b00cfa9fb3624a
廖浩龙 <aliaohaolong@gmail.com>
2026-07-14 23:09:36 +0800

    feat: 新增全局响应拦截器

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import axios from "axios";
+import { useAuthStore } from "@/stores/auth.ts";
 
 const app = createApp(App)
 
@@ -23,3 +24,22 @@ app.mount('#app')
 
 axios.defaults.baseURL = 'http://192.168.1.110:8080'
 axios.defaults.timeout = 5000
+
+// 全局响应拦截器
+axios.interceptors.response.use(
+  // onFulfilled
+  (response) => {
+
+    // 检查响应是否包含 JWT（即使为空字符串）
+    if (response.data?.data?.jwt !== undefined) {
+      const authStore = useAuthStore();
+      authStore.setJwt(response.data.data.jwt); // 更新 JWT
+    }
+
+    return response;
+  },
+  // onRejected
+  (error) => {
+    return Promise.reject(error);
+  }
+);
