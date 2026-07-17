4a590ff83bc4074784abf9ff635120d903abdbbc
廖浩龙 <aliaohaolong@gmail.com>
2026-07-17 21:03:04 +0800

    feat: 启用 mock

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from "@tailwindcss/vite";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    viteMockServe({
+      mockPath: "mock",
+      enable: true,
+    })
   ],
   resolve: {
     alias: {
