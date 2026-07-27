f79efb48d88eb9e2d0bdb4549b984f0513ab0af3
廖浩龙 <aliaohaolong@gmail.com>
2026-07-27 18:07:33 +0800

    feat: 新增 confirmDisabled 接口属性用于控制确认按钮是否禁用

--- src/components/popup/Popup.vue
+++ src/components/popup/Popup.vue
@@ -8,6 +8,10 @@ const popupKey = defineModel<PopupKey>("popupKey", { required: true });
 const title = defineModel<string>("title", { required: false, default: '标题' });
 const confirm = defineModel<string>("confirm", { required: false, default: '确定' });
 const confirmType = defineModel<'default' | 'danger'>("confirmType", { required: false, default: 'default' });
+// 接收只读参数
+defineProps({
+  confirmDisabled: Boolean,
+})
 // 定义事件（向外发送）
 const emit = defineEmits<{
   confirm: [];
@@ -45,14 +49,14 @@ const popupStore = usePopupStore();
         <button type="button" @click="popupStore.close()" tabindex="0" data-slot="button" class="group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none focus-visible:border-(--ring) focus-visible:ring-3 focus-visible:ring-(--ring)/50 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-(--destructive) aria-invalid:ring-3 aria-invalid:ring-(--destructive)/20 dark:aria-invalid:border-(--destructive)/50 dark:aria-invalid:ring-(--destructive)/40 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0 [&amp;_svg:not([class*='size-'])]:size-4 hover:bg-(--muted) hover:text-(--foreground) aria-expanded:bg-(--muted) aria-expanded:text-(--foreground) dark:hover:bg-(--muted)/50 h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2">
           取消
         </button>
-        <button v-if="confirmType === 'default'" type="button" @click="emit('confirm')" tabindex="0" data-slot="button" class="group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none
+        <button v-if="confirmType === 'default'" type="button" @click="emit('confirm')" :disabled="confirmDisabled" tabindex="0" data-slot="button" class="group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none
           focus-visible:border-(--ring) focus-visible:ring-3 focus-visible:ring-(--ring)/50
           active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-(--destructive) aria-invalid:ring-3 aria-invalid:ring-(--destructive)/20 dark:aria-invalid:border-(--destructive)/50 dark:aria-invalid:ring-(--destructive)/40 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0 [&amp;_svg:not([class*='size-'])]:size-4
           bg-(--primary) text-(--primary-foreground) hover:bg-(--primary)/80
           h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2">
           {{ confirm }}
         </button>
-        <button v-if="confirmType === 'danger'" type="button" @click="emit('confirm')" tabindex="0" data-slot="button" class="group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none
+        <button v-if="confirmType === 'danger'" type="button" @click="emit('confirm')" :disabled="confirmDisabled" tabindex="0" data-slot="button" class="group/button inline-flex shrink-0 items-center justify-center rounded-lg border border-transparent bg-clip-padding text-sm font-medium whitespace-nowrap transition-all outline-none select-none
           focus-visible:border-(--destructive)/40 focus-visible:ring-3 focus-visible:ring-(--destructive)/20
           active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-(--destructive) aria-invalid:ring-3 aria-invalid:ring-(--destructive)/20 dark:aria-invalid:border-(--destructive)/50 dark:aria-invalid:ring-(--destructive)/40 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0 [&amp;_svg:not([class*='size-'])]:size-4
           bg-(--destructive)/10 text-(--destructive) hover:bg-(--destructive)/20
