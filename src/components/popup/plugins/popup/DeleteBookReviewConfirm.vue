baae8bbd1dadcd53d9a5045e0e87008182f15b61
廖浩龙 <aliaohaolong@gmail.com>
2026-08-12 21:36:18 +0800

    fix: 修复已废弃的参数传递方式

--- src/components/popup/plugins/popup/DeleteBookReviewConfirm.vue
+++ src/components/popup/plugins/popup/DeleteBookReviewConfirm.vue
@@ -2,19 +2,27 @@
 import { usePopupStore } from "@/stores/popup.ts";
 import Popup from "@/components/popup/core/Popup.vue";
 import { useUserStore } from "@/stores/user.ts";
+import { onMounted, ref } from "vue";
 
 const userStore = useUserStore();
-const popupStore = usePopupStore();
+
+const user_name = ref<string>('');
 
 function confirmHandler() {
 }
+
+onMounted(() => {
+  usePopupStore().registerInitHook('deleteBookReviewConfirm', ({ clone }) => {
+    user_name.value = clone.user_name;
+  })
+})
 </script>
 
 <template>
   <Popup popup-key="deleteBookReviewConfirm" title="删除书评" confirm="确认删除" confirm-type="danger" @confirm="confirmHandler">
     <template #content>
       <template v-if="userStore.user_is_admin">
-        确定要删除 {{ popupStore.payload.user_name }} 的书评吗？此操作不可撤销。
+        确定要删除 {{ user_name }} 的书评吗？此操作不可撤销。
       </template>
       <template v-else>
         确定要删除您的书评吗？此操作不可撤销。
