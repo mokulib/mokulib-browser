8436b2c1edc6539d314aee759bbaeed71a33b7a7
廖浩龙 <aliaohaolong@gmail.com>
2026-08-13 20:07:07 +0800

    feat: 添加个人资料页面横幅

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -14,12 +14,12 @@ const navItems = [
 <template>
   <main class="flex-1">
     <!-- 横幅（仅宽屏） -->
-    <div class="hidden mx-auto max-w-6xl sm:flex px-4 md:px-8 py-12">
-      横幅
+    <div class="hidden mx-auto max-w-6xl sm:flex mb-4 md:mb-8 px-4 md:px-8">
+      <img src="/banner.png" class="w-full h-full rounded-b-lg" alt="横幅"/>
     </div>
 
     <!-- 主区域 -->
-    <div class="mx-auto max-w-6xl flex sm:px-4 pb-4 md:px-8 md:pb-8 gap-4 md:gap-8">
+    <div class="mx-auto max-w-6xl flex sm:px-4 md:px-8 pb-4 md:pb-8 gap-4 md:gap-8">
       <!-- 路由（仅宽屏） -->
       <aside class="hidden sm:flex flex-col mx-auto max-w-6xl gap-2">
         <template v-for="navItem in navItems" :key="navItem.route">
