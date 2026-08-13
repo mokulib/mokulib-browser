d225a7d9bdd654a5c1b77233bbc9b5e12ca1099a
廖浩龙 <aliaohaolong@gmail.com>
2026-08-13 21:31:31 +0800

    feat: 完成安全中心页面页面设计（功能未实现）

---
+++ src/components/security/SecurityCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+  title: { type: String, required: true },
+})
+</script>
+
+<template>
+  <div class="flex flex-col py-4 md:py-8 gap-4 border-b border-(--border) last:border-b-0">
+    <p class="font-semibold">{{ title }}</p>
+    <div class="flex px-4 md:px-8">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
