b6d78916822864dd27898dac7fb06b8259612247
廖浩龙 <aliaohaolong@gmail.com>
2026-08-13 17:56:25 +0800

    feat: 使用真实借阅历史数据

--- src/views/profile/ProfileMyView.vue
+++ src/views/profile/ProfileMyView.vue
@@ -10,8 +10,10 @@ import type { Book } from "@/types";
 
 const userStore = useUserStore();
 
-const borrowing = ref<{ id: number, title: string }[]>([])
+const borrowing = ref<{ id: number, title: string }[]>([]);
 const favorites = ref<{ id: number, title: string }[]>([]);
+const history = ref<{ id: number, title: string }[]>([]);
+const books = ref<Book[]>([]);
 
 onMounted(async () => {
   // 请求借阅中的数据
@@ -25,6 +27,23 @@ onMounted(async () => {
   const favorites_ = (await api.get<Book[]>("/api/users/favorites")).data;
   // 将数据转换为 { id: number, title: string }[] 格式
   favorites.value = favorites_.map(book => ({ id: book.id as number, title: book.title as string }))
+  // 请求借阅历史数据
+  const history_ = (await api.get<any[]>('/api/users/history')).data;
+  // 收集待查询的图书 id （去掉已查过的图书）
+  const bookIds = history_.map(record => record.book_id as number).filter(bookId => !books.value.some(book => bookId === book.id));
+  // 构造批量查询
+  const bookPromises = bookIds.map(bookId => api.get<Book>(`/api/books/${bookId}`));
+  // 并发请求数据
+  const data = await Promise.all(bookPromises);
+  // 获取结果
+  const books_ = data.map(d => d.data);
+  // 追加保存
+  books.value.push(...books_);
+  // 构造历史记录
+  history.value = history_.map(record => ({
+    id: record.book_id,
+    title: books.value.find(book => book.id === record.book_id)!.title
+  }));
 })
 </script>
 
@@ -62,7 +81,7 @@ onMounted(async () => {
     <div class="hidden sm:flex gap-3 md:gap-4">
       <RouteCard title="我的借阅" route="profile-borrowing" empty-text="当前没有借阅" :empty-icon="BookOpen" :book-list="borrowing"/>
       <RouteCard title="好书收藏" route="profile-favorite" empty-text="还没有收藏图书" :empty-icon="FolderHeart" :book-list="favorites"/>
-      <RouteCard title="借阅过的好书" route="profile-history" empty-text="没有借阅记录" :empty-icon="Folders" :book-list="[]"/>
+      <RouteCard title="借阅过的好书" route="profile-history" empty-text="没有借阅记录" :empty-icon="Folders" :book-list="history"/>
     </div>
   </div>
 </template>
