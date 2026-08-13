379c19c280f4d65d1b17299bbd3bad9e7ad7f0a0
廖浩龙 <aliaohaolong@gmail.com>
2026-08-13 13:58:44 +0800

    fix: 修复错误的布局设计导致子路由重复挂载

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -12,17 +12,16 @@ const navItems = [
 </script>
 
 <template>
-  <!-- 宽屏：横幅 + (路由 + 组件) -->
-  <main class="flex-1 hidden sm:block">
-    <!-- 横幅 -->
-    <div class="mx-auto max-w-6xl flex px-4 md:px-8 py-12">
+  <main class="flex-1">
+    <!-- 横幅（仅宽屏） -->
+    <div class="hidden mx-auto max-w-6xl sm:flex px-4 md:px-8 py-12">
       横幅
     </div>
 
     <!-- 主区域 -->
-    <div class="mx-auto max-w-6xl flex px-4 pb-4 md:px-8 md:pb-8 gap-4 md:gap-8">
-      <!-- 路由 -->
-      <aside class="flex flex-col mx-auto max-w-6xl gap-2">
+    <div class="mx-auto max-w-6xl flex sm:px-4 pb-4 md:px-8 md:pb-8 gap-4 md:gap-8">
+      <!-- 路由（仅宽屏） -->
+      <aside class="hidden sm:flex flex-col mx-auto max-w-6xl gap-2">
         <template v-for="navItem in navItems" :key="navItem.route">
           <!-- 按钮 -->
           <RouterLink :to="{ name: navItem.route }"
@@ -38,11 +37,6 @@ const navItems = [
         <RouterView/>
       </section>
     </div>
-  </main>
-
-  <!-- 窄屏：组件 -->
-  <main class="flex-1 block sm:hidden">
-    <RouterView/>
   </main>
 </template>
 
