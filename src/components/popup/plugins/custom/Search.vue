6c0ee59f0c2c9ca64ebfa14ed7977a2deccd06ed
廖浩龙 <aliaohaolong@gmail.com>
2026-08-15 15:16:24 +0800

    feat: 添加热搜功能；重构界面布局；优化搜索结果卡片交互逻辑；优化若干指针状态

--- src/components/popup/plugins/custom/Search.vue
+++ src/components/popup/plugins/custom/Search.vue
@@ -5,21 +5,29 @@ import { usePopupStore } from "@/stores/popup.ts";
 import type { Book } from "@/types";
 import api from "@/api";
 import type { Page } from "@/types/page.ts";
+import { useRouter } from "vue-router";
 
 // 定义类型
 type SortMode = "PUBLISH_DATE_FROM_NEW_TO_OLD" | "PUBLISH_DATE_FROM_OLD_TO_NEW" | "PRICE_FROM_LOW_TO_HIGH" | "PRICE_FROM_HIGH_TO_LOW";
 type Conditions = { keyword: string, sortMode: SortMode };
 
+const router = useRouter();
 const popupStore = usePopupStore();
 
 const isSearched = ref(false);
 const isLoading = ref(false);
 const requestTimestamp = ref(0);
 const searchInput = ref("");
+const hotSearches = ["英语", "数学", "计算机"];
 const conditions = ref<Conditions>({ keyword: "", sortMode: "PUBLISH_DATE_FROM_NEW_TO_OLD" })
-const results = ref<Page<Book>>({ current: 0, pages: 0, records: [], size: 0, total: 0 });
 const isPublishDateSort = computed(() => conditions.value.sortMode === "PUBLISH_DATE_FROM_NEW_TO_OLD" || conditions.value.sortMode === "PUBLISH_DATE_FROM_OLD_TO_NEW");
 const isPriceSort = computed(() => conditions.value.sortMode === "PRICE_FROM_LOW_TO_HIGH" || conditions.value.sortMode === "PRICE_FROM_HIGH_TO_LOW");
+const results = ref<Page<Book>>({ current: 0, pages: 0, records: [], size: 0, total: 0 });
+
+async function hotSearch(keyword: string) {
+  searchInput.value = keyword;
+  await goToPage(1, 'PUBLISH_DATE_FROM_NEW_TO_OLD');
+}
 
 async function goToPage(pageNum: number, sortMode_: SortMode) {
   if (isSearched.value && searchInput.value === conditions.value.keyword && sortMode_ === conditions.value.sortMode && pageNum === results.value.current)
@@ -57,73 +65,88 @@ onMounted(() => popupStore.registerInitHook('search', ({ clone }) => {
 </script>
 
 <template>
-  <div v-if="popupStore.isOpen('search')" class="absolute w-full h-full md:max-w-2xl flex flex-col items-center justify-start pt-8 bg-(--background) md:bg-transparent animate-in fade-in-0 md:slide-in-from-top duration-0 md:duration-300">
-    <!-- 输入框 -->
-    <div class="w-full flex items-center mb-8 px-8 gap-3 md:rounded-2xl bg-(--background)">
-      <button @click="popupStore.close()" class="md:hidden">
-        <ArrowLeft class="size-5 text-(--secondary-foreground)"/>
-      </button>
-      <div class="w-full flex items-center px-4 md:px-0 py-2 gap-3 rounded-2xl border border-(--border) md:border-none">
-        <button @click="goToPage(1, 'PUBLISH_DATE_FROM_NEW_TO_OLD')">
-          <Search class="size-5 text-(--secondary-foreground)"/>
+  <div v-if="popupStore.isOpen('search')" class="absolute top-0 w-full h-full md:h-auto md:max-w-2xl flex flex-col items-center justify-start pt-0 md:pt-8 animate-in fade-in-0 md:slide-in-from-top duration-300">
+    <!-- 背景 -->
+    <div class="w-full h-full flex flex-col items-center mb-0 md:mb-8 p-4 md:rounded-lg bg-(--background)">
+      <!-- 返回 + 输入 -->
+      <div class="w-full flex items-center gap-3 bg-(--background)">
+        <button @click="popupStore.close()" class="md:hidden mx-2">
+          <ArrowLeft class="size-5 text-(--secondary-foreground)"/>
         </button>
-        <input v-model="searchInput" type="text" @keydown.enter="goToPage(1, 'PUBLISH_DATE_FROM_NEW_TO_OLD')" placeholder="搜索..." class="w-full text-(--secondary-foreground) outline-none"/>
-        <Loader class="size-5 text-(--secondary-foreground) opacity-0 animate-spin" :class="{ 'opacity-100': isLoading }" style="transition-duration: 500ms; animation-duration: 3000ms"/>
+        <div class="w-full px-6 md:px-8 rounded border border-(--border)">
+          <div class="w-full flex items-center py-2 gap-3">
+            <button @click="goToPage(1, 'PUBLISH_DATE_FROM_NEW_TO_OLD')">
+              <Search class="size-5 text-(--secondary-foreground)"/>
+            </button>
+            <input v-model="searchInput" type="text" @keydown.enter="goToPage(1, 'PUBLISH_DATE_FROM_NEW_TO_OLD')" placeholder="搜索..." class="w-full text-(--secondary-foreground) outline-none"/>
+            <Loader class="size-5 text-(--secondary-foreground) opacity-0 animate-spin" :class="{ 'opacity-100': isLoading }" style="transition-duration: 500ms; animation-duration: 3000ms"/>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <!-- 无搜索结果 -->
-    <div v-if="isSearched && results.total === 0" class="w-full flex flex-col items-center justify-center p-10 rounded-2xl text-(--secondary-foreground) bg-(--background)">
-      <div>未找到相关结果</div>
-    </div>
+      <!-- 热搜 -->
+      <div class="w-full flex items-center justify-start mt-2 pl-18 md:pl-8 pr-16 text-sm">
+        <div class="text-(--muted-foreground)">热搜：</div>
+        <div class="flex items-center justify-center gap-2">
+          <template v-for="search in hotSearches" :key="search">
+            <div @click="hotSearch(search)" class="cursor-pointer text-(--muted-foreground) hover:text-(--primary) hover:underline">{{ search }}</div>
+          </template>
+        </div>
+      </div>
 
-    <!-- 有搜索结果 -->
-    <div v-if="isSearched && results.total > 0" class="w-full flex flex-col rounded-2xl text-(--secondary-foreground) bg-(--background)">
-      <!-- Header -->
-      <div class="flex items-center justify-between px-10 pt-3 pb-2.5 border-b border-(--border)">
+      <!-- 无搜索结果 -->
+      <div v-if="isSearched && results.total === 0" class="mt-6 p-6 rounded-2xl bg-(--background)">
+        <div class="text-sm tracking-wider">未找到相关结果</div>
+      </div>
+
+      <!-- 有搜索结果 - Header -->
+      <div v-if="isSearched && results.total > 0" class="w-full flex items-center justify-between mt-6 mb-2 px-8 py-1 rounded text-(--muted-foreground) bg-(--muted)">
         <!-- 排序 -->
         <div class="flex items-center gap-2">
-          <button @click="() => goToPage(1, conditions.sortMode === 'PUBLISH_DATE_FROM_NEW_TO_OLD' ? 'PUBLISH_DATE_FROM_OLD_TO_NEW' : 'PUBLISH_DATE_FROM_NEW_TO_OLD')" class="flex items-center gap-2" :class="{ 'text-(--primary)': isPublishDateSort }">
+          <button @click="() => goToPage(1, conditions.sortMode === 'PUBLISH_DATE_FROM_NEW_TO_OLD' ? 'PUBLISH_DATE_FROM_OLD_TO_NEW' : 'PUBLISH_DATE_FROM_NEW_TO_OLD')" class="flex items-center gap-2 cursor-pointer" :class="{ 'text-(--primary)': isPublishDateSort }">
             <span class="text-sm">出版时间</span>
             <ArrowDownNarrowWide v-if="isPriceSort || conditions.sortMode === 'PUBLISH_DATE_FROM_NEW_TO_OLD'" class="size-4"/>
             <ArrowUpNarrowWide v-if="conditions.sortMode === 'PUBLISH_DATE_FROM_OLD_TO_NEW'" class="size-4"/>
           </button>
-          <div class="w-px h-full bg-(--border)"></div>
-          <button @click="() => goToPage(1, conditions.sortMode === 'PRICE_FROM_LOW_TO_HIGH' ? 'PRICE_FROM_HIGH_TO_LOW' : 'PRICE_FROM_LOW_TO_HIGH')" class="flex items-center gap-2" :class="{ 'text-(--primary)': isPriceSort }">
+          <div class="w-px h-10px block bg-(--border)"></div>
+          <button @click="() => goToPage(1, conditions.sortMode === 'PRICE_FROM_LOW_TO_HIGH' ? 'PRICE_FROM_HIGH_TO_LOW' : 'PRICE_FROM_LOW_TO_HIGH')" class="flex items-center gap-2 cursor-pointer" :class="{ 'text-(--primary)': isPriceSort }">
             <span class="text-sm">价格</span>
             <ArrowDownNarrowWide v-if="isPublishDateSort || conditions.sortMode === 'PRICE_FROM_LOW_TO_HIGH'" class="size-4"/>
             <ArrowUpNarrowWide v-if="conditions.sortMode === 'PRICE_FROM_HIGH_TO_LOW'" class="size-4"/>
           </button>
         </div>
         <!-- 统计 -->
-        <div class="text-sm text-(--muted-foreground)">共 {{ results.total }} 个结果</div>
+        <div class="text-sm">共 {{ results.total }} 个结果</div>
       </div>
+
       <!-- 搜索结果 -->
-      <div class="flex-1 overflow-y-auto overscroll-behavior-contain min-h-32 max-h-[calc(100vh-192px)] md:max-h-[calc(100vh-222px)]">
-        <div class="flex flex-col p-4 gap-4">
+      <div v-if="isSearched && results.total > 0" class="w-full overflow-y-auto overscroll-behavior-contain min-h-30 max-h-[calc(100vh-190px)] md:max-h-[calc(100vh-255px)]">
+        <div class="flex flex-col gap-2">
           <template v-for="result in results.records" :key="result.id">
-            <div class="flex">
-              <img :src="`/books/${result.id}`" class="size-24 rounded-2xl" alt="Book Cover"/>
-              <div class="ml-4">
-                <div class="flex">
-                  <RouterLink :to="`/book/${result.id}`" class="mt-0.5 line-clamp-1 text-sm text-(--secondary-foreground) hover:text-(--primary) hover:underline">{{ result.title }}</RouterLink>
-                </div>
-                <div class="mt-0.5 line-clamp-1 text-xs text-(--muted-foreground)">{{ result.author }} / {{ result.publish_date }} / {{ result.publisher }} / {{ result.price.toFixed(2) }}￥</div>
-                <div class="mt-3.5 line-clamp-2 text-sm text-(--secondary-foreground)">{{ result.description }}</div>
+            <div @click="router.push({ name: 'book', params: { id: result.id } })" class="
+              flex p-2 gap-4 border border-(--border) rounded md:hover:border-(--primary) cursor-pointer
+              md:[&_.title]:text-(--muted-foreground)/50 md:hover:[&_.title]:text-(--foreground)
+              [&_.desc]:text-(--muted-foreground) md:[&_.desc]:text-(--muted-foreground)/50 md:hover:[&_.desc]:text-(--muted-foreground)">
+              <img :src="`/books/${result.id}`" class="size-24 object-cover rounded-lg md:rounded" alt="Book Cover"/>
+              <div class="flex flex-col items-start">
+                <div class="title mt-0.5 line-clamp-1 text-sm">{{ result.title }}</div>
+                <div class="desc mt-0.5 line-clamp-1 text-xs">{{ result.author }} / {{ result.publish_date }} / {{ result.publisher }} / {{ result.price.toFixed(2) }}￥</div>
+                <div class="desc mt-3.5 line-clamp-2 text-sm">{{ result.description }}</div>
               </div>
             </div>
           </template>
         </div>
       </div>
+
       <!-- 分页组件 -->
-      <div v-if="results.total > 0" class="flex items-center justify-center pt-2.5 pb-3 gap-2 rounded-b-2xl border-t border-(--border)">
-        <button @click="goToPage(1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50" :disabled="results.current === 1">首页</button>
-        <button @click="goToPage(results.current - 1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50" :disabled="results.current === 1">上一页</button>
+      <div v-if="isSearched && results.total > 0" class="flex items-center justify-center mt-2 gap-2">
+        <button @click="goToPage(1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50 cursor-pointer disabled:cursor-auto" :disabled="results.current === 1">首页</button>
+        <button @click="goToPage(results.current - 1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50 cursor-pointer disabled:cursor-auto" :disabled="results.current === 1">上一页</button>
         <template v-for="i in results.pages">
-          <button @click="goToPage(i, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary)" :class="{ 'text-(--primary)': results.current === i }">{{ i }}</button>
+          <button @click="results.current === i ? null : goToPage(i, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) cursor-pointer data-[is-current=true]:cursor-auto" :data-is-current="results.current === i" :class="{ 'text-(--primary)': results.current === i }">{{ i }}</button>
         </template>
-        <button @click="goToPage(results.current + 1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50" :disabled="results.current === results.pages">下一页</button>
-        <button @click="goToPage(results.pages, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50" :disabled="results.current === results.pages">尾页</button>
+        <button @click="goToPage(results.current + 1, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50 cursor-pointer disabled:cursor-auto" :disabled="results.current === results.pages">下一页</button>
+        <button @click="goToPage(results.pages, conditions.sortMode)" class="text-sm text-(--foreground) hover:text-(--primary) disabled:text-(--muted-foreground)/50 cursor-pointer disabled:cursor-auto" :disabled="results.current === results.pages">尾页</button>
       </div>
     </div>
   </div>
