98551c168c6f0b77b956f3aa131b878c6012c40a
廖浩龙 <aliaohaolong@gmail.com>
2026-08-15 16:24:16 +0800

    feat: 添加弹窗自动聚焦功能；优先聚焦弹窗内的 data-autofocus 元素；为弹窗添加 ESC 键关闭

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView, useRoute } from 'vue-router';
 import PageHeader from "@/components/PageHeader.vue";
 import PageFooter from "@/components/PageFooter.vue";
-import { computed } from "vue";
+import { computed, nextTick, ref, watch } from "vue";
 import { usePopupStore } from "@/stores/popup.ts";
 
 const route = useRoute();
@@ -10,8 +10,21 @@ const popupStore = usePopupStore();
 
 const hideHeader = computed(() => route.meta.hideHeader ?? false);
 const hideFooter = computed(() => route.meta.hideFooter ?? false);
-
+const popupRef = ref<HTMLElement>();
 const popupPlugins = Object.values(import.meta.glob('./components/popup/plugins/**/*.vue', { eager: true })).map((mod: any) => mod.default);
+
+watch(() => popupStore.isOpen(), (newValue) => {
+  if (newValue) {
+    nextTick(() => {
+      // 在 popupRef 范围内查找标记了 data-autofocus 的元素
+      const autofocusEl = popupRef.value?.querySelector('[data-autofocus]') as HTMLElement | null;
+      // 优先聚焦标记了 data-autofocus 的元素
+      if (autofocusEl) autofocusEl.focus();
+      // 否则聚焦整个弹窗
+      else             popupRef.value?.focus();
+    });
+  }
+})
 </script>
 
 <template>
@@ -28,7 +41,7 @@ const popupPlugins = Object.values(import.meta.glob('./components/popup/plugins/
   </div>
 
   <!-- 弹窗遮罩 -->
-  <div v-show="popupStore.isOpen()" @click.self="popupStore.close()" class="fixed inset-0 z-50 bg-black/66 animate-in fade-in-0 flex items-end justify-center sm:items-center" :class="{ 'sm:bg-transparent': popupStore.isOpen('header') }">
+  <div ref="popupRef" v-show="popupStore.isOpen()" @click.self="popupStore.close()" @keydown.esc="popupStore.close()" tabindex="-1" class="fixed inset-0 z-50 bg-black/66 animate-in fade-in-0 flex items-end justify-center sm:items-center" :class="{ 'sm:bg-transparent': popupStore.isOpen('header') }">
     <!-- 自动挂载预设 Popup 弹窗 -->
     <component v-for="(plugin, index) in popupPlugins" :key="index" :is="plugin"/>
   </div>
