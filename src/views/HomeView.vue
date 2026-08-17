9c3c0f16afd5bf705b1ccf110bcfb9be2c1a54a6
廖浩龙 <aliaohaolong@gmail.com>
2026-08-17 22:16:40 +0800

    refactor: 提取排行榜组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import api from "@/api";
 import type { Page } from "@/types/page.ts";
 import { useBookStore } from "@/stores/book.ts";
 import { useRouter } from "vue-router";
+import RankCard from "@/components/home/RankCard.vue";
 
 type BaseStatus = { isActive: boolean };
 type HotStatus = BaseStatus & { type: 'hot' };
@@ -140,49 +141,9 @@ onMounted(async () => {
           <!-- 热门好书 -->
           <div v-if="isHotActive" class="grid grid-rows-1 grid-cols-2 pl-4 pt-4 gap-4">
             <!-- 阅读榜 -->
-            <div class="flex flex-col p-4 rounded-lg bg-[url('/bg-card.png')] dark:bg-[url('/bg-card-dark.png')] bg-contain bg-top bg-no-repeat bg-white dark:bg-black shadow-[0_12px_67px_0_rgba(0,0,0,0.04)]">
-              <div class="text-xl tracking-wider">阅读榜单</div>
-              <div class="mt-1 mb-4 text-xs text-(--muted-foreground)/60">仅展示前十本，统计时间截止至04-01 24:00</div>
-              <div class="flex flex-col gap-1" :class="{ 'pb-2': activatedBorrowedIndex === 0 }">
-                <template v-for="(bookId, index) in topBorrowedBookIds" :key="bookId">
-                  <div v-if="activatedBorrowedIndex !== index" @click="activatedFavoriteIndex = index" @mouseenter="activatedBorrowedIndex = index" class="flex items-center mt-2 cursor-pointer">
-                    <div class="text-sm font-semibold text-(--muted-foreground)/50" :class="{ 'text-[#FF5F00]': index === 0, 'text-[#00754A]': index === 1, 'text-[#D6A562]': index === 2 }">{{ String(index + 1).padStart(2, '0') }}</div>
-                    <div class="line-clamp-1 ml-2 text-sm">{{ bookStore.book(bookId).value?.title }}</div>
-                  </div>
-                  <div v-if="activatedBorrowedIndex === index" @click="router.push({ name: 'book', params: { id: bookId } })" class="flex mt-8 first:mt-6 p-4 rounded-lg bg-white dark:bg-black shadow cursor-pointer">
-                    <div class="text-sm font-semibold text-(--muted-foreground)/50" :class="{ 'text-[#FF5F00]': index === 0, 'text-[#00754A]': index === 1, 'text-[#D6A562]': index === 2 }">{{ String(index + 1).padStart(2, '0') }}</div>
-                    <div class="flex-1 flex flex-col mx-2 gap-1">
-                      <div class="line-clamp-1 text-sm">{{ bookStore.book(bookId).value?.title }}</div>
-                      <div class="line-clamp-1 text-xs">{{ bookStore.book(bookId).value?.author }}</div>
-                      <div class="line-clamp-2 text-xs">{{ bookStore.book(bookId).value?.description }}</div>
-                    </div>
-                    <img :src="`/books/${bookId}`" class="w-26 aspect-3/4 object-cover -mt-12 -mb-2 rounded shadow" :alt="bookStore.book(bookId).value?.title"/>
-                  </div>
-                </template>
-              </div>
-            </div>
+            <RankCard title="阅读榜单" background-image="bg-card.png" background-image-dark="bg-card-dark.png" :rank="topBorrowedBookIds"/>
             <!-- 收藏榜 -->
-            <div class="flex flex-col p-4 rounded-lg bg-[url('/bg-card-2.png')] dark:bg-[url('/bg-card-2-dark.png')] bg-contain bg-top bg-no-repeat bg-white dark:bg-black shadow-[0_12px_67px_0_rgba(0,0,0,0.04)]">
-              <div class="text-xl tracking-wider">收藏榜单</div>
-              <div class="mt-1 mb-4 text-xs text-(--muted-foreground)/60">仅展示前十本，统计时间截止至04-01 24:00</div>
-              <div class="flex flex-col gap-1" :class="{ 'pb-2': activatedFavoriteIndex === 0 }">
-                <template v-for="(bookId, index) in topFavoriteBookIds" :key="bookId">
-                  <div v-if="activatedFavoriteIndex !== index" @click="activatedFavoriteIndex = index" @mouseenter="activatedFavoriteIndex = index" class="flex items-center mt-2 cursor-pointer">
-                    <div class="text-sm font-semibold text-(--muted-foreground)/50" :class="{ 'text-[#FF5F00]': index === 0, 'text-[#00754A]': index === 1, 'text-[#D6A562]': index === 2 }">{{ String(index + 1).padStart(2, '0') }}</div>
-                    <div class="line-clamp-1 ml-2 text-sm">{{ bookStore.book(bookId).value?.title }}</div>
-                  </div>
-                  <div v-if="activatedFavoriteIndex === index" @click="router.push({ name: 'book', params: { id: bookId } })" class="flex mt-8 first:mt-6 p-4 rounded-lg bg-white dark:bg-black shadow cursor-pointer">
-                    <div class="text-sm font-semibold text-(--muted-foreground)/50" :class="{ 'text-[#FF5F00]': index === 0, 'text-[#00754A]': index === 1, 'text-[#D6A562]': index === 2 }">{{ String(index + 1).padStart(2, '0') }}</div>
-                    <div class="flex-1 flex flex-col mx-2 gap-2">
-                      <div class="line-clamp-1 text-sm">{{ bookStore.book(bookId).value?.title }}</div>
-                      <div class="line-clamp-1 text-xs">{{ bookStore.book(bookId).value?.author }}</div>
-                      <div class="line-clamp-2 text-xs">{{ bookStore.book(bookId).value?.description }}</div>
-                    </div>
-                    <img :src="`/books/${bookId}`" class="w-26 aspect-3/4 object-cover -mt-12 -mb-2 rounded shadow" :alt="bookStore.book(bookId).value?.title"/>
-                  </div>
-                </template>
-              </div>
-            </div>
+            <RankCard title="收藏榜单" background-image="bg-card-2.png" background-image-dark="bg-card-2-dark.png" :rank="topFavoriteBookIds"/>
           </div>
           <!-- 新书上架 -->
           <div v-if="isNewActive">
