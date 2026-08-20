185728c618e8b160ae51adb96995abd714212712
廖浩龙 <aliaohaolong@gmail.com>
2026-08-20 13:50:54 +0800

    feat: 实现注销账号功能

--- src/components/security/CloseAccountCard.vue
+++ src/components/security/CloseAccountCard.vue
@@ -4,16 +4,38 @@ import { ref } from "vue";
 import SecurityCard from "@/components/security/SecurityCard.vue";
 import { useUserStore } from "@/stores/user.ts";
 import FormItem from "@/components/security/FormItem.vue";
+import api from "@/api";
+import { ElMessage } from "element-plus";
 
 const userStore = useUserStore();
 
-const input = ref('')
+const prefix = ref('--');
+const captcha = ref('');
+
+async function requestCaptcha() {
+  const data = await api.get('/api/auth/close-account');
+  prefix.value = data.data.code_prefix;
+  if (data.status === 'OK') {
+    ElMessage.success(data.message);
+  } else {
+    ElMessage.error(data.message);
+  }
+}
+
+async function closeAccount() {
+  const data = await api.delete('/api/auth/close-account', { params: { emailCaptcha: prefix.value + '-' + captcha.value, } });
+  if (data.status === 'OK') {
+    ElMessage.success(data.message);
+  } else {
+    ElMessage.error(data.message);
+  }
+}
 </script>
 
 <template>
   <SecurityCard title="注销账号">
     <!-- 提示信息 -->
-    <div class="flex flex-col px-4 py-2 gap-2 rounded text-sm text-(--destructive) border border-(--destructive)">
+    <div class="flex flex-col px-4 py-2 gap-2 rounded text-sm text-(--destructive) border border-(--destructive) bg-(--card)">
       <p>为保证账号安全，你提交的注销申请生效前需满足以下条件。</p>
       <ul class="list-decimal pl-8">
         <li>通过账号安全验证</li>
@@ -25,11 +47,11 @@ const input = ref('')
 
     <FormItem label="验证码" :description="`验证码将发送至您的注册邮箱（${userStore.user_email}）。`">
       <div class="sm:max-w-64 flex-1 flex items-center mr-4 py-1 border border-(--border) rounded text-sm">
-        <p class="shrink-0 w-10 text-center">--</p>
+        <p class="shrink-0 w-10 text-center">{{ prefix }}</p>
         <p class="shrink-0 w-6 text-center border-x border-(--border)">-</p>
-        <input v-model="input" type="text" placeholder="请输入验证码" class="w-full px-4 outline-none"/>
+        <input v-model="captcha" type="text" placeholder="请输入验证码" class="w-full px-4 outline-none"/>
       </div>
-      <button class="shrink-0 flex items-center px-4 py-1 gap-2 border border-(--border) rounded text-sm bg-(--background) hover:bg-(--muted) transition-colors">
+      <button @click="requestCaptcha()" class="shrink-0 flex items-center px-4 py-1 gap-2 border border-(--border) rounded text-sm bg-(--background) hover:bg-(--muted) transition-colors">
         <Send class="size-3"/>
         发送验证码
       </button>
@@ -37,7 +59,7 @@ const input = ref('')
 
     <!-- 操作按钮 -->
     <div>
-      <button class="flex items-center px-4 py-1 gap-2 border border-(--destructive) rounded text-sm text-(--destructive) bg-(--background) hover:bg-(--muted) transition-colors">
+      <button @click="closeAccount()" class="flex items-center px-4 py-1 gap-2 border border-(--border) rounded text-sm text-(--destructive) bg-(--background) hover:bg-(--muted) transition-colors">
         <UserRoundX class="size-3"/>
         注销账号
       </button>
