6e9e307b9c6bb065cfa3bc0e273d46695d4ef470
廖浩龙 <aliaohaolong@gmail.com>
2026-08-29 20:56:28 +0800

    feat: config 参数适配类型安全

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios';
 import type { Response } from '@/types';
 
 const index = {
-  post: async <T = any>(url: string, data?: any, config?: any): Promise<Response<T>> => {
+  post: async <T = any, D = any>(url: string, data?: any, config?: AxiosRequestConfig<D>): Promise<Response<T>> => {
     const response = await axios.post(url, data, config);
     return response.data as Response<T>;
   },
 
-  delete: async <T = any>(url: string, config?: any): Promise<Response<T>> => {
+  delete: async <T = any, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<Response<T>> => {
     const response = await axios.delete(url, config);
     return response.data as Response<T>;
   },
 
-  put: async <T = any>(url: string, data?: any, config?: any): Promise<Response<T>> => {
+  put: async <T = any, D = any>(url: string, data?: any, config?: AxiosRequestConfig<D>): Promise<Response<T>> => {
     const response = await axios.put(url, data, config);
     return response.data as Response<T>;
   },
 
-  get: async <T = any>(url: string, config?: any): Promise<Response<T>> => {
+  get: async <T = any, D = any>(url: string, config?: AxiosRequestConfig<D>): Promise<Response<T>> => {
     const response = await axios.get(url, config);
     return response.data as Response<T>;
   },
