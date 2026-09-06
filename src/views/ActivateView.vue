a46acceba0db08e927fdaa5b216bd87c4f3b10aa
廖浩龙 <aliaohaolong@gmail.com>
2026-09-06 16:48:19 +0800

    refactor: 移除 Element Plus Loading服务依赖优化激活页面状态管理和UI显示；添加激活状态下的旋转动画效果

--- src/views/ActivateView.vue
+++ src/views/ActivateView.vue
@@ -1,28 +1,36 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { ElLoading } from "element-plus";
-import { CircleAlert, CircleCheck, CircleX, GlobeX } from "@lucide/vue";
+import { computed, onMounted, ref } from "vue";
+import { Loader, CircleCheck, CircleX, GlobeX } from "@lucide/vue";
 import api from "@/api";
 
 const { token } = defineProps(['token'])
 
 const status = ref<'ACTIVATING' | 'OK' | 'ERROR' | 'NETWORK_ERROR'>('ACTIVATING');
+const statusClass = computed(() => {
+  switch (status.value) {
+    case 'ACTIVATING': return 'text-(--muted-foreground)'
+    case 'OK': return 'text-green-700'
+    case 'ERROR': return 'text-red-700'
+    case 'NETWORK_ERROR': return 'text-(--muted-foreground)'
+  }
+})
+const statusIcon = computed(() => {
+  switch (status.value) {
+    case 'ACTIVATING': return Loader
+    case 'OK': return CircleCheck
+    case 'ERROR': return CircleX
+    case 'NETWORK_ERROR': return GlobeX
+  }
+})
 const message = ref('激活中，请稍候');
 
 onMounted(async () => {
-  // 设置加载遮罩
-  const loading = ElLoading.service({
-    lock: true,
-    text: '',
-    background: 'rgba(0, 0, 0, 0.5)',
-  })
   // 请求
   const data = await api.post('/api/auth/activate/' + token);
   // 处理（增加延迟以避免屏幕闪烁）
   setTimeout(() => {
     status.value = data.status as 'OK' | 'ERROR' | 'NETWORK_ERROR'
     message.value = data.message
-    loading.close()
   }, 1000)
 })
 </script>
@@ -33,24 +41,9 @@ onMounted(async () => {
       <h1 class="font-serif text-2xl text-(--muted-foreground)">账户激活</h1>
     </div>
 
-    <div v-if="status === 'ACTIVATING'" class="flex flex-col items-center gap-4 text-(--muted-foreground)">
-      <CircleAlert class="size-14"/>
-      <p class="font-serif text-2xl font-semibold">{{ message }}</p>
-    </div>
-
-    <div v-if="status === 'OK'" class="flex flex-col items-center gap-4 text-green-700">
-      <CircleCheck class="size-14"/>
-      <p class="font-serif text-2xl font-semibold">{{ message }}</p>
-    </div>
-
-    <div v-if="status === 'ERROR'" class="flex flex-col items-center gap-4 text-red-700">
-      <CircleX class="size-14"/>
-      <p class="font-serif text-2xl font-semibold">{{ message }}</p>
-    </div>
-
-    <div v-if="status === 'NETWORK_ERROR'" class="flex flex-col items-center gap-4 text-(--muted-foreground)">
-      <GlobeX class="size-14"/>
-      <p class="font-serif text-2xl font-semibold">{{ message }}</p>
+    <div class="flex flex-col items-center gap-4 text-(--muted-foreground)">
+      <status-icon class="size-14" :class="[ statusClass, status === 'ACTIVATING' ? 'animate-spin [animation-duration:3s]' : '' ]"/>
+      <p class="font-serif text-2xl font-semibold" :class="statusClass">{{ message }}</p>
     </div>
 
     <div class="mt-6 pt-1 px-1 border-t border-(--border) text-center text-sm text-(--muted-foreground)">
