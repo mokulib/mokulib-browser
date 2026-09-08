3a9756cbdd0fed4b9cf5f89bef0e4d02d90362c2
廖浩龙 <aliaohaolong@gmail.com>
2026-09-08 17:45:10 +0800

    fix: 修复错误的提示文本；受后端接口调整影响，将参数从对象改为数组形式

--- src/components/popup/plugins/popup/EditCategory.vue
+++ src/components/popup/plugins/popup/EditCategory.vue
@@ -19,7 +19,7 @@ async function confirmHandler() {
   let category;
   // 创建分类
   if (customCategory.value)
-    category = (await api.post<Category>('/api/categories', { name: customCategory.value } )).data
+    category = (await api.post<Category>('/api/categories', [ customCategory.value ] )).data
   else
     category = selectedCategory.value;
   // 提交请求
@@ -63,7 +63,7 @@ onMounted(() => {
       </div>
       <div class="space-y-1.5">
         <label data-slot="label" class="flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50" for="custom-tag">
-          自定义标签
+          新建分类
         </label>
         <input v-model="customCategoryInput" data-slot="input" placeholder="输入分类名" class="h-8 w-full min-w-0 rounded-lg border border-(--input) bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-(--foreground) placeholder:text-(--muted-foreground) focus-visible:border-(--ring) focus-visible:ring-3 focus-visible:ring-(--ring)/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-(--input)/50 disabled:opacity-50 aria-invalid:border-(--destructive) aria-invalid:ring-3 aria-invalid:ring-(--destructive)/20 md:text-sm dark:bg-(--input)/30 dark:disabled:bg-(--input)/80 dark:aria-invalid:border-(--destructive)/50 dark:aria-invalid:ring-(--destructive)/40" value="">
       </div>
