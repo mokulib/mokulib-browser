d0144b1d0468ffdbf3a4ba3a8be302fb9af11c48
廖浩龙 <aliaohaolong@gmail.com>
2026-09-23 23:38:03 +0800

    feat: 添加页面标题管理功能；添加 meta 定义

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from "@/stores/auth.ts";
 import { usePopupStore } from "@/stores/popup.ts";
+import { useBookStore } from "@/stores/book.ts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,7 @@ const router = createRouter({
       name: 'login',
       component: () => import('@/views/LoginView.vue'),
       meta: {
+        title: '登录 - 墨库',
         requiresAuth: false, // 使用局部路由守卫进行验证，禁用全局路由守卫验证
         hideHeader: true,
         hideFooter: true,
@@ -35,11 +37,17 @@ const router = createRouter({
       path: '/terms',
       name: 'terms',
       component: () => import('@/views/TermsView.vue'),
+      meta: {
+        title: '用户协议 - 墨库',
+      },
     },
     {
       path: '/privacy',
       name: 'privacy',
       component: () => import('@/views/PrivacyView.vue'),
+      meta: {
+        title: '隐私政策 - 墨库',
+      },
     },
     // 激活账户页
     {
@@ -47,6 +55,9 @@ const router = createRouter({
       name: 'activate',
       component: () => import('@/views/ActivateView.vue'),
       props: true, // 将路径参数作为 props 传递给组件
+      meta: {
+        title: '激活账户 - 墨库',
+      },
       // 局部路由守卫
       async beforeEnter() {
         const authStore = useAuthStore();
@@ -71,21 +82,33 @@ const router = createRouter({
           path: 'my',
           name: 'profile-my',
           component: () => import('@/views/profile/ProfileMyView.vue'),
+          meta: {
+            title: '我的 - 墨库',
+          },
         },
         {
           path: 'borrowing',
           name: 'profile-borrowing',
           component: () => import('@/views/profile/ProfileBorrowingView.vue'),
+          meta: {
+            title: '我的借阅 - 墨库',
+          },
         },
         {
           path: 'favorite',
           name: 'profile-favorite',
           component: () => import('@/views/profile/ProfileFavoriteView.vue'),
+          meta: {
+            title: '好书收藏 - 墨库',
+          },
         },
         {
           path: 'history',
           name: 'profile-history',
           component: () => import('@/views/profile/ProfileHistoryView.vue'),
+          meta: {
+            title: '借阅历史 - 墨库',
+          },
         },
       ],
     },
@@ -94,6 +117,7 @@ const router = createRouter({
       name: 'security',
       component: () => import('@/views/SecurityView.vue'),
       meta: {
+        title: '账户与安全 - 墨库',
         requiresAuth: true,
         redirectToHomeOnLogout: true,
       },
@@ -103,6 +127,18 @@ const router = createRouter({
       name: 'book',
       component: () => import('@/views/BookView.vue'),
       props: true, // 将路径参数作为 props 传递给组件
+      meta: {
+        title: async (to) => {
+          // 获取 pinia 存储
+          const bookStore = useBookStore();
+          // 预加载
+          await bookStore.preload(Number(to.params.id));
+          // 获取图书信息
+          const book = bookStore.book(Number(to.params.id)).value;
+          // 返回标题
+          return book?.title ? `${book.title} - 墨库` : '图书不存在 - 墨库'
+        }
+      },
     },
     {
       path: '/borrow-record/:id',
@@ -119,6 +155,7 @@ const router = createRouter({
       name: 'dashboard',
       component: () => import('@/views/DashboardView.vue'),
       meta: {
+        title: '数据概览 - 墨库',
         requiresAuth: true,
         redirectToHomeOnLogout: true,
       },
@@ -128,12 +165,22 @@ const router = createRouter({
       path: '/:pathMatch(.*)*', // 通配符路由
       name: 'not-found',
       component: () => import('@/views/NotFoundView.vue'),
+      meta: {
+        title: '404 - 墨库',
+      },
     }
   ],
 })
 
 // 全局路由守卫
 router.beforeEach(async (to) => {
+  // 设置页面标题
+  if (typeof to.meta.title === 'function')
+    document.title = await to.meta.title(to)
+  else if (typeof to.meta.title === 'string')
+    document.title = to.meta.title
+  else
+    document.title = '墨库 · 个人图书馆'
   // 关闭弹窗
   usePopupStore().close();
   // 根据需要验证登录状态
---
+++ src/types/router.d.ts
@@ -0,0 +1,17 @@
+import 'vue-router'
+import type { RouteLocationNormalized } from 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    // 标题
+    title?: string | ((to: RouteLocationNormalized) => string) | ((to: RouteLocationNormalized) => Promise<string>)
+    // 是否隐藏 Header
+    hideHeader?: boolean
+    // 是否隐藏 Footer
+    hideFooter?: boolean
+    // 是否需要登录
+    requiresAuth?: boolean
+    // 在未登录的情况下，是否需要强制跳转到登录页面
+    redirectToHomeOnLogout?: boolean
+  }
+}
